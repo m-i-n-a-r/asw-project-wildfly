7d63ef622cf8717ef588da4b4af5530940f6f38c
Zefyrus94 <giacomo.bonanni94@gmail.com>
2018-06-30 19:42:36 +0200

    ambiente e script installazione java maven wildfly

---
+++ environments/_shared/scripts/common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# dove vengono montate le risorse e i download condivisi 
+ASW_HOME=/home/asw-project-wildfly 
+ASW_DOWNLOADS=${ASW_HOME}/_shared/downloads
+ASW_RESOURCES=${ASW_HOME}/_shared/resources
+
+PROJECT_FOLDER=/home/vagrant/project
